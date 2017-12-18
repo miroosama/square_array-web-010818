f6acdc270c368a7690e6a75303f3c84cc44567b4
osama miro <o.miro650@gmail.com>
2017-12-18 00:03:43 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
   new_numbers = []
-  array.each 
-    new_numbers.push(x **2)
+  array.each do
+    |new_numbers.push(x **2)|
 
   end
 return new_numbers
